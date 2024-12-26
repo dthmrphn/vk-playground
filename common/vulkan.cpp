ff8e61c8e567e676ee516f4bf17618392a2e268d
user <dzmitry.kolas@regula.by>
2024-12-26 15:17:44 +0300

    comon lib updated

---
+++ common/vulkan.cpp
@@ -0,0 +1,241 @@
+#include "vulkan.hpp"
+
+#include <set>
+
+#include <fmt/core.h>
+
+namespace vulkan {
+
+VKAPI_ATTR VkBool32 VKAPI_CALL device::debug_callback(VkDebugUtilsMessageSeverityFlagBitsEXT message_severity,
+                                                      VkDebugUtilsMessageTypeFlagsEXT message_types,
+                                                      VkDebugUtilsMessengerCallbackDataEXT const* callback_data,
+                                                      void* user_data) {
+    fmt::print("{}\n", callback_data->pMessage);
+    return false;
+}
+
+device::device(const vk::ApplicationInfo& app_info,
+               const std::vector<const char*>& layers,
+               const std::vector<const char*>& extensions,
+               vk::QueueFlags queues, bool debug) : device() {
+    _instance = {
+        _context,
+        vk::InstanceCreateInfo{{}, &app_info, layers, extensions},
+    };
+
+    _physical_dev = vk::raii::PhysicalDevices{_instance}.front();
+
+    if (debug) {
+        using severity = vk::DebugUtilsMessageSeverityFlagBitsEXT;
+        using type = vk::DebugUtilsMessageTypeFlagBitsEXT;
+        constexpr auto sev_flags{severity::eWarning | severity::eError};
+        constexpr auto msg_flags{type::eGeneral | type::ePerformance | type::eValidation};
+        vk::DebugUtilsMessengerCreateInfoEXT debug_ci({}, sev_flags, msg_flags, &device::debug_callback);
+
+        _dbg_msgr = {_instance, debug_ci};
+    }
+
+    std::set<std::uint32_t> indices{};
+
+    if (queues & vk::QueueFlagBits::eGraphics) {
+        const auto index = queue_family_index(vk::QueueFlagBits::eGraphics);
+        indices.insert(index);
+    }
+
+    if (queues & vk::QueueFlagBits::eCompute) {
+        const auto index = queue_family_index(vk::QueueFlagBits::eCompute);
+        indices.insert(index);
+    }
+
+    if (queues & vk::QueueFlagBits::eTransfer) {
+        const auto index = queue_family_index(vk::QueueFlagBits::eTransfer);
+        indices.insert(index);
+    }
+
+    const auto priority{1.0f};
+    std::vector<vk::DeviceQueueCreateInfo> queue_ci;
+    for (const auto i : indices) {
+        queue_ci.emplace_back(vk::DeviceQueueCreateFlags(), i, 1, &priority);
+    }
+
+    vk::DeviceCreateInfo device_ci{{}, queue_ci, layers, extensions};
+    _logical_dev = {_physical_dev, device_ci};
+}
+
+std::uint32_t device::queue_family_index(vk::QueueFlags flags) const {
+    const auto props = _physical_dev.getQueueFamilyProperties();
+    const auto iter = std::find_if(props.begin(), props.end(), [flags](auto p) {
+        return p.queueFlags & flags;
+    });
+
+    if (iter == props.end()) {
+        throw std::runtime_error(fmt::format("failed to get {} queue index", vk::to_string(flags)));
+    }
+
+    return std::distance(props.begin(), iter);
+}
+
+std::uint32_t device::memory_type_index(std::uint32_t filter, vk::MemoryPropertyFlags mask) const {
+    const auto props = _physical_dev.getMemoryProperties();
+    for (std::uint32_t i = 0; i < props.memoryTypeCount; ++i) {
+        if (filter & (1 << i) && ((props.memoryTypes[i].propertyFlags & mask) == mask)) {
+            return i;
+        }
+    }
+    throw std::runtime_error("failed to find memory type");
+}
+
+vk::raii::Queue device::make_graphic_queue() const {
+    const auto i = queue_family_index(vk::QueueFlagBits::eGraphics);
+    return _logical_dev.getQueue(i, 0);
+}
+
+vk::raii::Queue device::make_compute_queue() const {
+    const auto i = queue_family_index(vk::QueueFlagBits::eCompute);
+    return _logical_dev.getQueue(i, 0);
+}
+
+vk::raii::Queue device::make_present_queue() const {
+    return {nullptr};
+}
+
+const vk::raii::PhysicalDevice& device::physical() const {
+    return _physical_dev;
+}
+
+const vk::raii::Instance& device::instance() const {
+    return _instance;
+}
+
+const vk::raii::Device& device::logical() const {
+    return _logical_dev;
+}
+
+buffer::buffer(const device& device, vk::DeviceSize size, vk::BufferUsageFlags usage, vk::MemoryPropertyFlags mask) {
+    vk::BufferCreateInfo ci{{}, size, usage, vk::SharingMode::eExclusive};
+    _buf = {device.logical(), ci};
+
+    const auto req = _buf.getMemoryRequirements();
+    const auto index = device.memory_type_index(req.memoryTypeBits, mask);
+
+    vk::MemoryAllocateInfo ai{req.size, index};
+    _mem = {device.logical(), ai};
+    _buf.bindMemory(_mem, 0);
+}
+
+const vk::raii::Buffer& buffer::buf() const {
+    return _buf;
+}
+
+const vk::raii::DeviceMemory& buffer::mem() const {
+    return _mem;
+}
+
+device_buffer::device_buffer(const device& device, vk::DeviceSize size, vk::BufferUsageFlags usage)
+    : buffer(device, size, usage, vk::MemoryPropertyFlagBits::eDeviceLocal) {}
+
+host_buffer::host_buffer(const device& device, vk::DeviceSize size, vk::BufferUsageFlags usage)
+    : buffer(device, size, usage, vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent) {
+    _mapped = _mem.mapMemory(0, size);
+}
+
+texture::texture(const device& device, std::uint32_t width, std::uint32_t height) {
+    vk::ImageCreateInfo ici{
+        {},
+        vk::ImageType::e2D,
+        vk::Format::eR8G8B8A8Srgb,
+        {width, height, 1},
+        1,
+        1,
+        vk::SampleCountFlagBits::e1,
+        vk::ImageTiling::eOptimal,
+        vk::ImageUsageFlagBits::eSampled | vk::ImageUsageFlagBits::eTransferDst,
+        vk::SharingMode::eExclusive,
+    };
+
+    _img = {device.logical(), ici};
+
+    const auto req = _img.getMemoryRequirements();
+    const auto index = device.memory_type_index(req.memoryTypeBits, vk::MemoryPropertyFlagBits::eDeviceLocal);
+
+    vk::MemoryAllocateInfo mai{req.size, index};
+    _mem = {device.logical(), mai};
+    _img.bindMemory(_mem, 0);
+
+    vk::ImageViewCreateInfo ivci{
+        {},
+        _img,
+        vk::ImageViewType::e2D,
+        vk::Format::eR8G8B8A8Srgb,
+        {},
+        {vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1},
+    };
+    _view = {device.logical(), ivci};
+
+    vk::SamplerCreateInfo sic{
+        {},
+        vk::Filter::eLinear,
+        vk::Filter::eLinear,
+        {},
+    };
+
+    _sampler = {device.logical(), sic};
+}
+
+swapchain::swapchain(const device& device, const vk::SurfaceKHR& surf, std::uint32_t w, std::uint32_t h) {
+    _surface = {device.instance(), surf};
+
+    const auto formats = device.physical().getSurfaceFormatsKHR(_surface);
+    _format = formats[0];
+    for (const auto& f : formats) {
+        if (f.format == vk::Format::eB8G8R8A8Unorm && f.colorSpace == vk::ColorSpaceKHR::eSrgbNonlinear) {
+            _format = f;
+        }
+    }
+
+    const auto capabilities = device.physical().getSurfaceCapabilitiesKHR(_surface);
+    _extent = vk::Extent2D{
+        std::clamp(w, capabilities.minImageExtent.width, capabilities.maxImageExtent.width),
+        std::clamp(h, capabilities.minImageExtent.height, capabilities.maxImageExtent.height),
+    };
+
+    const auto pre_transform = capabilities.currentTransform;
+    const auto composite_alpha = vk::CompositeAlphaFlagBitsKHR::eOpaque;
+    const auto present_mode = vk::PresentModeKHR::eMailbox;
+
+    vk::SwapchainCreateInfoKHR sci{
+        {},
+        _surface,
+        capabilities.minImageCount + 1,
+        _format.format,
+        _format.colorSpace,
+        _extent,
+        1,
+        vk::ImageUsageFlagBits::eColorAttachment,
+        vk::SharingMode::eExclusive,
+        nullptr,
+        pre_transform,
+        composite_alpha,
+        present_mode,
+        true,
+    };
+
+    _swapchain = {device.logical(), sci};
+
+    vk::ImageViewCreateInfo ci{
+        {},
+        {},
+        vk::ImageViewType::e2D,
+        _format.format,
+        {vk::ComponentSwizzle::eIdentity, vk::ComponentSwizzle::eIdentity, vk::ComponentSwizzle::eIdentity, vk::ComponentSwizzle::eIdentity},
+        {vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1},
+    };
+
+    const auto images = _swapchain.getImages();
+    for (const auto& image : images) {
+        ci.image = image;
+        _image_views.emplace_back(device.logical(), ci);
+    }
+}
+
+} // namespace vulkan
---
+++ common/vulkan.hpp
@@ -0,0 +1,94 @@
+#pragma once
+
+#include <cstdint>
+
+#include <vulkan/vulkan_raii.hpp>
+
+namespace vulkan {
+
+class device {
+    vk::raii::Context _context;
+    vk::raii::Instance _instance{nullptr};
+    vk::raii::DebugUtilsMessengerEXT _dbg_msgr{nullptr};
+    vk::raii::PhysicalDevice _physical_dev{nullptr};
+    vk::raii::Device _logical_dev{nullptr};
+
+  public:
+    device() = default;
+    device(const vk::ApplicationInfo& app_info,
+           const std::vector<const char*>& layers,
+           const std::vector<const char*>& extensions,
+           vk::QueueFlags queues, bool debug);
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debug_callback(VkDebugUtilsMessageSeverityFlagBitsEXT,
+                                                         VkDebugUtilsMessageTypeFlagsEXT,
+                                                         const VkDebugUtilsMessengerCallbackDataEXT*,
+                                                         void*);
+
+    std::uint32_t queue_family_index(vk::QueueFlags flags) const;
+    std::uint32_t memory_type_index(std::uint32_t filter, vk::MemoryPropertyFlags mask) const;
+
+    const vk::raii::PhysicalDevice& physical() const;
+    const vk::raii::Instance& instance() const;
+    const vk::raii::Device& logical() const;
+
+    vk::raii::Queue make_graphic_queue() const;
+    vk::raii::Queue make_present_queue() const;
+    vk::raii::Queue make_compute_queue() const;
+};
+
+class buffer {
+  protected:
+    vk::raii::Buffer _buf{nullptr};
+    vk::raii::DeviceMemory _mem{nullptr};
+
+  public:
+    buffer() = default;
+    buffer(const device& dev, vk::DeviceSize size, vk::BufferUsageFlags usage, vk::MemoryPropertyFlags mask);
+
+    const vk::raii::Buffer& buf() const;
+    const vk::raii::DeviceMemory& mem() const;
+};
+
+class device_buffer : public buffer {
+  public:
+    device_buffer() = default;
+    device_buffer(const device& dev, vk::DeviceSize size, vk::BufferUsageFlags usage);
+};
+
+class host_buffer : public buffer {
+    void* _mapped{nullptr};
+
+  public:
+    host_buffer() = default;
+    host_buffer(const device& dev, vk::DeviceSize size, vk::BufferUsageFlags usage);
+};
+
+class texture {
+    vk::raii::Image _img{nullptr};
+    vk::raii::ImageView _view{nullptr};
+    vk::raii::DeviceMemory _mem{nullptr};
+    vk::raii::Sampler _sampler{nullptr};
+
+  public:
+    texture() = default;
+    texture(const device& device, std::uint32_t width, std::uint32_t height);
+
+    static constexpr vk::DescriptorSetLayoutBinding layout_binding(std::uint32_t binding) {
+        return {binding, vk::DescriptorType::eCombinedImageSampler, 1, vk::ShaderStageFlagBits::eFragment};
+    }
+};
+
+class swapchain {
+    vk::raii::SurfaceKHR _surface{nullptr};
+    vk::raii::SwapchainKHR _swapchain{nullptr};
+    vk::SurfaceFormatKHR _format{};
+    vk::Extent2D _extent{};
+    std::vector<vk::raii::ImageView> _image_views{};
+
+  public:
+    swapchain() = default;
+    swapchain(const device& dev, const vk::SurfaceKHR& surf, std::uint32_t w, std::uint32_t h);
+};
+
+} // namespace vulkan
