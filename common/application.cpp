432afcbaca6634b334b09b02ea8dcd9f4952a079
dthmrphn <freemansdubz@gmail.com>
2024-12-28 17:28:59 +0300

    added double buffering

--- common/application.cpp
+++ common/application.cpp
@@ -45,8 +45,8 @@ application::application(const vk::ApplicationInfo& app_info, std::uint32_t w, s
     vk::CommandPoolCreateInfo ci{flags, _graphic_queue_index};
     _command_pool = {_device.logical(), ci};
 
-    vk::CommandBufferAllocateInfo ai{_command_pool, vk::CommandBufferLevel::ePrimary, 1};
-    _command_buffer = {std::move(vk::raii::CommandBuffers{_device.logical(), ai}.front())};
+    vk::CommandBufferAllocateInfo ai{_command_pool, vk::CommandBufferLevel::ePrimary, frames_in_flight};
+    _command_buffers = vk::raii::CommandBuffers{_device.logical(), ai};
 
     // render pass creation
     vk::AttachmentDescription attachment_desc{
@@ -93,45 +93,56 @@ application::application(const vk::ApplicationInfo& app_info, std::uint32_t w, s
     }
 
     // synchronization creation
+    //_image_available_semaphores.resize(frames_in_flight, nullptr);
+    //_render_finished_semaphores.resize(frames_in_flight, nullptr);
+    //_fences.resize(frames_in_flight, nullptr);
     vk::SemaphoreCreateInfo sci{};
-    _image_available_semaphore = {_device.logical(), sci};
-    _render_finished_semaphore = {_device.logical(), sci};
-
     vk::FenceCreateInfo fci{vk::FenceCreateFlagBits::eSignaled};
-    _fence = {_device.logical(), fci};
+    for (std::size_t i = 0; i < frames_in_flight; ++i) {
+        _image_available_semaphores.emplace_back(_device.logical(), sci);
+        _render_finished_semaphores.emplace_back(_device.logical(), sci);
+        _fences.emplace_back(_device.logical(), fci);
+    }
 }
 
 void application::render() {
-    while (vk::Result::eTimeout == _device.logical().waitForFences(*_fence, vk::True, -1)) {
+    while (vk::Result::eTimeout == _device.logical().waitForFences(*_fences[_current_frame], vk::True, -1)) {
     }
-    _device.logical().resetFences(*_fence);
+    _device.logical().resetFences(*_fences[_current_frame]);
 
-    auto [rv, index] = _swapchain.get().acquireNextImage(-1, _image_available_semaphore);
+    auto [rv, index] = _swapchain.get().acquireNextImage(-1, _image_available_semaphores[_current_frame]);
     if (rv != vk::Result::eSuccess) {
         fmt::print("acquire err: {}\n", vk::to_string(rv));
     }
 
-    _command_buffer.reset();
-    _command_buffer.begin({});
+    _command_buffers[_current_frame].reset();
+    _command_buffers[_current_frame].begin({});
 
     vk::ClearValue clear_value{vk::ClearColorValue{0.5f, 0.5f, 0.5f, 1.0f}};
     vk::RenderPassBeginInfo rpbi{_render_pass, _framebuffers[index], {{0, 0}, _swapchain.extent()}, clear_value};
-    _command_buffer.beginRenderPass(rpbi, vk::SubpassContents::eInline);
+    _command_buffers[_current_frame].beginRenderPass(rpbi, vk::SubpassContents::eInline);
     vk::Viewport viewport{0.0f, 0.0f, (float)_swapchain.extent().width, (float)_swapchain.extent().height, 0.0f, 1.0f};
-    _command_buffer.setViewport(0, viewport);
-    _command_buffer.setScissor(0, vk::Rect2D{{0, 0}, _swapchain.extent()});
-    _command_buffer.endRenderPass();
-    _command_buffer.end();
+    _command_buffers[_current_frame].setViewport(0, viewport);
+    _command_buffers[_current_frame].setScissor(0, vk::Rect2D{{0, 0}, _swapchain.extent()});
+    _command_buffers[_current_frame].endRenderPass();
+    _command_buffers[_current_frame].end();
 
     vk::PipelineStageFlags wait_flags{vk::PipelineStageFlagBits::eColorAttachmentOutput};
-    vk::SubmitInfo submit{*_image_available_semaphore, wait_flags, *_command_buffer, *_render_finished_semaphore};
-    _graphic_queue.submit(submit, _fence);
+    vk::SubmitInfo submit{
+        *_image_available_semaphores[_current_frame],
+        wait_flags,
+        *_command_buffers[_current_frame],
+        *_render_finished_semaphores[_current_frame],
+    };
+    _graphic_queue.submit(submit, _fences[_current_frame]);
 
-    vk::PresentInfoKHR present_info{*_render_finished_semaphore, *_swapchain.get(), index};
+    vk::PresentInfoKHR present_info{*_render_finished_semaphores[_current_frame], *_swapchain.get(), index};
     rv = _present_queue.presentKHR(present_info);
     if (rv != vk::Result::eSuccess) {
         fmt::print("present err: {}\n", vk::to_string(rv));
     }
+
+    _current_frame = (_current_frame + 1) % frames_in_flight;
 }
 
 void application::resize_handler(GLFWwindow* win, int w, int h) {
@@ -141,7 +152,7 @@ void application::resize_handler(GLFWwindow* win, int w, int h) {
 
 void application::update_swapchain(std::uint32_t w, std::uint32_t h) {
     _device.logical().waitIdle();
-    
+
     _swapchain.resize(_device, w, h);
 
     _framebuffers.clear();
--- common/application.hpp
+++ common/application.hpp
@@ -8,6 +8,10 @@
 namespace common {
 
 class application {
+    static constexpr auto frames_in_flight{2};
+
+    std::size_t _current_frame{};
+
     vulkan::device _device;
     vulkan::swapchain _swapchain;
 
@@ -22,11 +26,10 @@ class application {
     std::vector<vk::raii::Framebuffer> _framebuffers{};
 
     vk::raii::CommandPool _command_pool{nullptr};
-    vk::raii::CommandBuffer _command_buffer{nullptr};
-
-    vk::raii::Semaphore _image_available_semaphore{nullptr};
-    vk::raii::Semaphore _render_finished_semaphore{nullptr};
-    vk::raii::Fence _fence{nullptr};
+    std::vector<vk::raii::CommandBuffer> _command_buffers;
+    std::vector<vk::raii::Semaphore> _image_available_semaphores;
+    std::vector<vk::raii::Semaphore> _render_finished_semaphores;
+    std::vector<vk::raii::Fence> _fences;
 
     GLFWwindow* _window;
 
