106d89e87662990be33b1e2147f474b1e98cacf4
dthmrphn <freemansdubz@gmail.com>
2025-01-10 15:54:59 +0300

    project restructuring, cmake udpated

--- cmake/spirv_library.cmake
+++ cmake/spirv_library.cmake
@@ -22,7 +22,7 @@ function(glsl2spirv glsl header namespace)
             "-DINPUT_FILE=${spirv_full}"
             "-DHEADER_FILE=${header}"
             "-DHEADER_NAMESPACE=${namespace}"
-            "-P ${CMAKE_CURRENT_LIST_DIR}/cmake/spirv2hpp.cmake"
+            "-P ${CMAKE_SOURCE_DIR}/cmake/spirv2hpp.cmake"
         DEPENDS ${spirv_full}
         COMMENT "generating header ${header}"
     )
@@ -30,21 +30,20 @@ endfunction()
 
 function(add_spirv_library name)
     cmake_parse_arguments(add_spirv_library "" "" "GLSL" ${ARGN})
+    set(header_dir "${CMAKE_CURRENT_BINARY_DIR}/${name}")
 
     add_library(${name} INTERFACE)
+    target_include_directories(${name} INTERFACE ${header_dir})
 
     foreach(source ${add_spirv_library_GLSL})
-        get_filename_component(directory ${source} DIRECTORY)
         get_filename_component(file_name ${source} NAME)
         set(source_full "${CMAKE_CURRENT_SOURCE_DIR}/${source}")
-        set(header_dir "${CMAKE_CURRENT_BINARY_DIR}/${directory}")
-        set(header "${header_dir}/${file_name}.hpp")
+        set(header_full "${header_dir}/${file_name}.hpp")
         string(REGEX REPLACE "\\." "_" namespace ${file_name})
 
-        glsl2spirv(${source_full} ${header} ${namespace})
+        glsl2spirv(${source_full} ${header_full} ${namespace})
 
-        target_sources(${name} INTERFACE ${header})
-        target_include_directories(${name} INTERFACE ${header_dir})
+        target_sources(${name} INTERFACE ${header_full})
     endforeach()
 
 endfunction()
